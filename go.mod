cd030c949d9704c67658327d5cea99a620d54092
Rocky <rockyzlwang@gmail.com>
2024-04-12 19:43:19 -0700

    Add files via upload

---
+++ go.mod
@@ -0,0 +1,62 @@
+module github.com/mdaxf/iac-mqtt
+
+go 1.21.4
+
+require github.com/mdaxf/iac v0.0.0-20240413000725-f4983d8490b7
+
+require (
+	github.com/IBM/sarama v1.43.0 // indirect
+	github.com/antonmedv/expr v1.15.3 // indirect
+	github.com/cenkalti/backoff/v4 v4.2.1 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/denisenkom/go-mssqldb v0.12.3 // indirect
+	github.com/dlclark/regexp2 v1.7.0 // indirect
+	github.com/dop251/goja v0.0.0-20231027120936-b396bb4c349d // indirect
+	github.com/eapache/go-resiliency v1.6.0 // indirect
+	github.com/eapache/go-xerial-snappy v0.0.0-20230731223053-c322873962e3 // indirect
+	github.com/eapache/queue v1.1.0 // indirect
+	github.com/eclipse/paho.mqtt.golang v1.4.3 // indirect
+	github.com/go-gomail/gomail v0.0.0-20160411212932-81ebce5c23df // indirect
+	github.com/go-kit/log v0.2.1 // indirect
+	github.com/go-logfmt/logfmt v0.5.1 // indirect
+	github.com/go-sourcemap/sourcemap v2.1.3+incompatible // indirect
+	github.com/go-sql-driver/mysql v1.7.1 // indirect
+	github.com/go-stomp/stomp v2.1.4+incompatible // indirect
+	github.com/golang-sql/civil v0.0.0-20190719163853-cb61b32ac6fe // indirect
+	github.com/golang-sql/sqlexp v0.1.0 // indirect
+	github.com/golang/snappy v0.0.4 // indirect
+	github.com/google/pprof v0.0.0-20230207041349-798e818bf904 // indirect
+	github.com/google/uuid v1.4.0 // indirect
+	github.com/gorilla/websocket v1.5.0 // indirect
+	github.com/hashicorp/errwrap v1.0.0 // indirect
+	github.com/hashicorp/go-multierror v1.1.1 // indirect
+	github.com/hashicorp/go-uuid v1.0.3 // indirect
+	github.com/jcmturner/aescts/v2 v2.0.0 // indirect
+	github.com/jcmturner/dnsutils/v2 v2.0.0 // indirect
+	github.com/jcmturner/gofork v1.7.6 // indirect
+	github.com/jcmturner/gokrb5/v8 v8.4.4 // indirect
+	github.com/jcmturner/rpc/v2 v2.0.3 // indirect
+	github.com/klauspost/compress v1.17.7 // indirect
+	github.com/mdaxf/signalrsrv v0.0.0-20231006234128-8da62fea4b77 // indirect
+	github.com/montanaflynn/stats v0.0.0-20171201202039-1bf9dbcd8cbe // indirect
+	github.com/pierrec/lz4/v4 v4.1.21 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/rcrowley/go-metrics v0.0.0-20201227073835-cf1acfcdf475 // indirect
+	github.com/robertkrimen/otto v0.2.1 // indirect
+	github.com/shiena/ansicolor v0.0.0-20230509054315-a9deabde6e02 // indirect
+	github.com/teivah/onecontext v1.3.0 // indirect
+	github.com/vmihailenco/msgpack/v5 v5.4.0 // indirect
+	github.com/vmihailenco/tagparser/v2 v2.0.0 // indirect
+	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
+	github.com/xdg-go/scram v1.1.2 // indirect
+	github.com/xdg-go/stringprep v1.0.4 // indirect
+	github.com/youmark/pkcs8 v0.0.0-20181117223130-1be2e3e5546d // indirect
+	go.mongodb.org/mongo-driver v1.12.1 // indirect
+	golang.org/x/crypto v0.19.0 // indirect
+	golang.org/x/net v0.21.0 // indirect
+	golang.org/x/sync v0.6.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
+	gopkg.in/alexcesaro/quotedprintable.v3 v3.0.0-20150716171945-2caba252f4dc // indirect
+	gopkg.in/sourcemap.v1 v1.0.5 // indirect
+	nhooyr.io/websocket v1.8.7 // indirect
+)
---
+++ main.go
@@ -0,0 +1,229 @@
+package main
+
+import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/mdaxf/iac/com"
+	"github.com/mdaxf/iac/config"
+	dbconn "github.com/mdaxf/iac/databases"
+	"github.com/mdaxf/iac/documents"
+	"github.com/mdaxf/iac/engine/trancode"
+	"github.com/mdaxf/iac/framework/callback_mgr"
+	"github.com/mdaxf/iac/integration/mqttclient"
+	"github.com/mdaxf/iac/logger"
+)
+
+func main() {
+	startTime := time.Now()
+
+	gconfig, err := config.LoadGlobalConfig()
+
+	if err != nil {
+		log.Fatalf("Failed to load global configuration: %v", err)
+		//ilog.Error(fmt.Sprintf("Failed to load global configuration: %v", err))
+	}
+	initializeloger(gconfig)
+
+	ilog := logger.Log{ModuleName: logger.Framework, User: "System", ControllerName: "iac-mqtt"}
+	ilog.Debug("Start the iac-mqtt")
+
+	DB := initializeDatabase(ilog, gconfig)
+	if DB == nil {
+		ilog.Error("Database connection error")
+		return
+	}
+
+	docDB := initializedDocuments(ilog, gconfig)
+	if docDB == nil {
+		ilog.Error("MongoDB connection error!")
+		return
+	}
+
+	if callback_mgr.CallBackMap["TranCode_Execute"] == nil {
+		ilog.Debug("Register the trancode execution interface")
+		tfr := trancode.TranFlowstr{}
+		callback_mgr.RegisterCallBack("TranCode_Execute", tfr.Execute)
+	}
+
+	initializeMqttClient(gconfig, ilog, DB, docDB)
+
+	elapsed := time.Since(startTime)
+	ilog.PerformanceWithDuration("iac-mqtt.main", elapsed)
+
+	//	waitForTerminationSignal()
+}
+
+func initializeloger(gconfig *config.GlobalConfig) error {
+	if gconfig.LogConfig == nil {
+		return fmt.Errorf("log configuration is missing")
+	}
+	fmt.Printf("log configuration: %v", gconfig.LogConfig)
+	logger.Init(gconfig.LogConfig)
+	return nil
+}
+
+func initializeMqttClient(gconfig *config.GlobalConfig, ilog logger.Log, DB *sql.DB, DocDB *documents.DocDB) {
+	startTime := time.Now()
+	defer func() {
+		elapsed := time.Since(startTime)
+		ilog.PerformanceWithDuration("main.initializeMqttClient", elapsed)
+	}()
+
+	config.MQTTClients = make(map[string]*mqttclient.MqttClient)
+
+	//	go func() {
+
+	ilog.Debug("initialize MQTT Client")
+
+	data, err := ioutil.ReadFile("mqttconfig.json")
+	if err != nil {
+		ilog.Debug(fmt.Sprintf("failed to read configuration file: %v", err))
+
+	}
+	ilog.Debug(fmt.Sprintf("MQTT Clients configuration file: %s", string(data)))
+	var mqttconfig mqttclient.MqttConfig
+	err = json.Unmarshal(data, &mqttconfig)
+	if err != nil {
+		ilog.Debug(fmt.Sprintf("failed to unmarshal the configuration file: %v", err))
+
+	}
+
+	ilog.Debug(fmt.Sprintf("MQTT Clients configuration: %v", logger.ConvertJson(mqttconfig)))
+	i := 1
+	for _, mqttcfg := range mqttconfig.Mqtts {
+		ilog.Debug(fmt.Sprintf("MQTT Client configuration: %s", logger.ConvertJson(mqttcfg)))
+		mqtc := mqttclient.NewMqttClient(mqttcfg)
+
+		mqtc.DB = DB
+		mqtc.DocDBconn = DocDB
+		mqtc.AppServer = com.ConverttoString(gconfig.AppServer["url"])
+		mqtc.ApiKey = "ahbcgj"
+		//config.MQTTClients[fmt.Sprintf("mqttclient_%d", i)] = mqtc
+		mqtc.Initialize_mqttClient()
+		//	fmt.Sprintln("MQTT Client: %v", config.MQTTClients)
+		//	ilog.Debug(fmt.Sprintf("MQTT Client: %v", config.MQTTClients))
+		i++
+	}
+
+	fmt.Println("MQTT Clients: %v, %d", config.MQTTClients, i)
+	ilog.Debug(fmt.Sprintf("MQTT Clients: %v, %d", config.MQTTClients, i))
+	//}()
+
+}
+
+func waitForTerminationSignal() {
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	<-c
+	fmt.Println("\nShutting down...")
+
+	time.Sleep(2 * time.Second) // Add any cleanup or graceful shutdown logic here
+	os.Exit(0)
+}
+
+func initializeDatabase(ilog logger.Log, gconfig *config.GlobalConfig) *sql.DB {
+	// function execution start time
+	startTime := time.Now()
+
+	// defer function to log the performance duration of initializeDatabase
+	defer func() {
+		elapsed := time.Since(startTime)
+		ilog.PerformanceWithDuration("main.initializeDatabase", elapsed)
+	}()
+
+	ilog.Debug("initialize Database")
+	databaseconfig := gconfig.DatabaseConfig
+
+	// check if database type is missing
+	if databaseconfig["type"] == nil {
+		ilog.Error(fmt.Sprintf("initialize Database error: %s", "DatabaseType is missing"))
+		return nil
+	}
+
+	// check if database connection is missing
+	if databaseconfig["connection"] == nil {
+		ilog.Error(fmt.Sprintf("initialize Database error: %s", "DatabaseConnection is missing"))
+		return nil
+	}
+
+	// set the database type and connection string
+	dbconn.DatabaseType = databaseconfig["type"].(string)
+	dbconn.DatabaseConnection = databaseconfig["connection"].(string)
+
+	// set the maximum idle connections, default to 5 if not provided or if the value is not a float64
+	if databaseconfig["maxidleconns"] == nil {
+		dbconn.MaxIdleConns = 5
+	} else {
+		if v, ok := databaseconfig["maxidleconns"].(float64); ok {
+			dbconn.MaxIdleConns = int(v)
+		} else {
+			dbconn.MaxIdleConns = 5
+		}
+	}
+
+	// set the maximum open connections, default to 10 if not provided or if the value is not a float64
+	if databaseconfig["maxopenconns"] == nil {
+		dbconn.MaxOpenConns = 10
+	} else {
+		if v, ok := databaseconfig["maxopenconns"].(float64); ok {
+			dbconn.MaxOpenConns = int(v)
+		} else {
+			dbconn.MaxOpenConns = 10
+		}
+	}
+
+	// connect to the database
+	err := dbconn.ConnectDB()
+	if err != nil {
+		ilog.Error(fmt.Sprintf("initialize Database error: %s", err.Error()))
+		return nil
+	}
+
+	return dbconn.DB
+}
+
+func initializedDocuments(ilog logger.Log, gconfig *config.GlobalConfig) *documents.DocDB {
+	startTime := time.Now()
+	defer func() {
+		elapsed := time.Since(startTime)
+		ilog.PerformanceWithDuration("main.initializedDocuments", elapsed)
+	}()
+
+	if gconfig.DocumentConfig == nil {
+		fmt.Errorf("documentdb configuration is missing")
+		return nil
+	}
+
+	documentsConfig := gconfig.DocumentConfig
+
+	ilog.Debug(fmt.Sprintf("initialize Documents, %v", documentsConfig))
+
+	var DatabaseType = documentsConfig["type"].(string)             // "mongodb"
+	var DatabaseConnection = documentsConfig["connection"].(string) //"mongodb://localhost:27017"
+	var DatabaseName = documentsConfig["database"].(string)         //"IAC_CFG"
+
+	if DatabaseType == "" {
+		ilog.Error(fmt.Sprintf("initialize Documents error: %s", "DatabaseType is missing"))
+		return nil
+	}
+	if DatabaseConnection == "" {
+		ilog.Error(fmt.Sprintf("initialize Documents error: %s", "DatabaseConnection is missing"))
+		return nil
+	}
+	if DatabaseName == "" {
+		ilog.Error(fmt.Sprintf("initialize Documents error: %s", "DatabaseName is missing"))
+		return nil
+	}
+
+	documents.ConnectDB(DatabaseType, DatabaseConnection, DatabaseName)
+
+	return documents.DocDBCon
+}
